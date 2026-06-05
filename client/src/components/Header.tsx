f6f8859ea2f419d328c849269231f2d0a82e15a7
JosephMelesse <josephmelesse1@gmail.com>
2026-06-05 16:06:16 -0700

    nav wireframe started

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header(){
     return (
-        <nav>
+        <nav className="flex items-center justify-between px-6 py-4 border-b">
+            <div className="font-medium">Joseph Melesse</div>
             <ul>
-                <li> Joseph Melesse </li>
                 <li> About </li>
                 <li> Projects </li>
                 <li> Contact </li>
