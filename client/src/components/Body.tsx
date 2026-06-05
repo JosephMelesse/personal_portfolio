f1443b669bdd562c4c8638f7ea0da4525131b6ab
JosephMelesse <josephmelesse1@gmail.com>
2026-06-05 13:59:59 -0700

    added a test print line

--- client/src/components/Body.tsx
+++ client/src/components/Body.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+console.log('API_URL is:', API_URL);
 
 export default function Body() {
   const [message, setMessage] = useState('');
