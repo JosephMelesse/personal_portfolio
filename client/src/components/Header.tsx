0e1e0785dd198ac6cf0f0c9cef83e233307d09e0
JosephMelesse <josephmelesse1@gmail.com>
2026-06-05 16:07:58 -0700

    added a flex on the nav

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header(){
     return (
         <nav className="flex items-center justify-between px-6 py-4 border-b">
             <div className="font-medium">Joseph Melesse</div>
-            <ul>
+            <ul className="flex gap-6">
                 <li> About </li>
                 <li> Projects </li>
                 <li> Contact </li>
